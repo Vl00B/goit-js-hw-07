e29e6c079db2c5fcc8a7e83d938193a035bbe753
Vl00B <vl.bezkrevny@ukr.net>
2022-08-31 14:29:13 +0300

    Updated. Added 'Escape' feature.

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -21,19 +21,42 @@ for (const item of galleryItems) {
 
 gallery.append(...arrayOfItems);
 
-for (let i = 1; i <= gallery.children.length; i += 1) {
-  const galleryLink = document.querySelector(
-    `.gallery__item:nth-of-type(${i}) .gallery__link`
-  );
+gallery.addEventListener('click', (event) => {
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+  event.preventDefault();
 
-  const source = galleryLink.getAttribute('href');
+  const source = event.target.dataset.source;
 
   const box = basicLightbox.create(
-    `<img width="1400" height="900" src="${source}">`
+    `<img width="1400" height="900" class="boxx" src="${source}" tabindex="0">`
   );
+  box.show();
 
-  galleryLink.addEventListener('click', (event) => {
-    event.preventDefault();
-    box.show();
-  });
-}
+  const specialFunc = function (event) {
+    console.log(event);
+    if (event.key === 'Escape') {
+      box.close();
+
+      document
+        .querySelector('body')
+        .removeEventListener('keydown', specialFunc);
+    }
+  };
+
+  document.querySelector('body').addEventListener('keydown', specialFunc);
+});
+
+// for (let i = 1; i <= gallery.children.length; i += 1) {
+//   const galleryLink = document.querySelector(
+//     `.gallery__item:nth-of-type(${i}) .gallery__link`
+//   );
+
+//   const source = galleryLink.getAttribute('href');
+
+//   galleryLink.addEventListener('click', (event) => {
+//     event.preventDefault();
+//     box.show();
+//   });
+// }
